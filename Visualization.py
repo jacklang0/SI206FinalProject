184230d8d4b26811703b4abbcf697f10174e799c
jacklang0 <jacklang@umich.edu>
2022-04-12 12:48:44 -0400

    Created file for visualizations

---
+++ Visualization.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import os
+import sqlite3
+
+#1. Pie chart: Wealth of US vs Top 100 billionaries
+
+#2. Bar graph: Count of Forbes 400 by Country, Split by Gender
+
+#3. Pie chart: Count of Forbes 400 by Industry 
+
+#4. Scatter plot: Number of Forbes 400 vs Gini Coef of Country
+
+#5. Scatter plot: Age vs Net Worth Amoung Forbes 400
+
+
